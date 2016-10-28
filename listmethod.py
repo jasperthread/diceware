b1610e279493825fadb7d8312ccac99c3df2edd9
jasperthread <dagburks@gmail.com>
2016-10-28 08:37:27 -0500

    Add files via upload

---
+++ listmethod.py
@@ -0,0 +1,15 @@
+import random
+
+thewords = []
+thephrase = ''
+
+with open('thewords_justwords.txt', 'r') as infile:
+    thewords = infile.read().split(', ')
+
+numwords = input('What length of keyphrase would you like?: ')
+
+for num in range(0, int(numwords)):
+    randomnumber = random.randint(0, len(thewords))
+    thephrase = thephrase + thewords[randomnumber] + ' '
+
+print(thephrase)
